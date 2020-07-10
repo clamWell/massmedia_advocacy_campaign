db22029086fcf15ed1c5b730d2a72af3c5b8f343
h62446244! <pepesi258@hanmail.net>
2020-07-10 14:51:55 +0900

    [보호종료아동_애드보카시] * PC퍼블리싱 - section04~05 작업중

--- js/custom.js
+++ js/custom.js
@@ -37,42 +37,50 @@ $(function(){
 	makeChr.makingCustomChr = function(){
 		
 	};
-
 	//대중매체 유사 캐릭터 반환
 	makeChr.findSimilarChr = function(){
 		
 	};
 	
 	*/
-
-		
 	
+	/*************make speach card***************/
+	var speachCard = {
+		data : s_card_data,
+		cardListHolder : $(".speach-list ul"),
+		printTemplate : function(i){
+			var d = this.data[i];
+			var n = randomRange(1, 7);
+			var template = ("<li class='spc-card'><div class='spc-card-wrap'><div class='card-index'><p>"+ ((i+1<10)? ("0"+(i+1)) : i+1) +"</p></div><div class='card-body card-body-0"+n+"'><div class='card-bg'><img src='img/s-c-"+((i+1<10)? ("0"+(i+1)): i+1)+".jpg' alt=''></div><div class='spc-text'><div class='quoto quoto-start'><img src='img/quoto-start.png' alt=''></div><div class='text-holder'><p>"+ d.spc +"</p></div><div class='quoto quoto-end'><img src='img/quoto-end.png' alt=''></div></div></div></div><div class='media-info'><p><span class='cont-name'>" + d.contName + "</span><span class='slash'>I</span><span class='maker'>"+ d.maker + "</span><span class='slash'>I</span><span class='year'>"+ d.year +"</span></p></div></li>");
+			return template;
+		},
+		makeCard : function(){
+			//document.querySelector(".speach-list ul").innerHTML = "";
+			this.cardListHolder.html("");
+			for(i=0;i<this.data.length;i++){
+				this.cardListHolder.append(this.printTemplate(i));
+			}
+		}
 
 
-	/******** 모바일 전용 조정 ********/
-	if(isMobile==true){
+	};
 
-	}
-	/******** 모바일 전용 조정 ********/
 
-	function init(){
-	
-	}
 
-	$(".loading-page").fadeOut(200, function(){
-		init();
-	});
-
+	/*******Video Slider function******/
 	var nowScroll;
 	var videoSlider = {
 		videoStatus : "before",
-		videoEndPoint : $(".fixed-slider-area").offset().top + $(".fixed-slider-area").height()-screenHeight,
+		sliderBody : $(".fixed-slider-area"),
+		videoEndPoint : function(){
+			return (this.sliderBody.offset().top + this.sliderBody.height()-screenHeight);
+		},
 		checkVideoStatus : function(sc){
-			if( sc >= $(".fixed-slider-area").offset().top && sc < this.videoEndPoint ){
+			if( sc >= this.sliderBody.offset().top && sc < this.videoEndPoint() ){
 				return "on";
-			}else if( sc < $(".fixed-slider-area").offset().top ){
+			}else if( sc < this.sliderBody.offset().top ){
 				return "before"
-			}else if( sc >= this.videoEndPoint ){
+			}else if( sc >= this.videoEndPoint() ){
 				return "after";
 			}
 		},
@@ -82,19 +90,21 @@ $(function(){
 			if(this.videoStatus == "on"){
 				$fixedEl.addClass("fixed-el-fixed");
 				$fixedEl.removeClass("fixed-el-bottom");
-				$(".fixed-slider-area").addClass("fixed-slider-area-bgBlack");
+				this.sliderBody.addClass("sder-bck");
 			}else if( this.videoStatus == "before"){
 				$fixedEl.removeClass("fixed-el-fixed");
 				$fixedEl.removeClass("fixed-el-bottom");
-				$(".fixed-slider-area").removeClass("fixed-slider-area-bgBlack");
+				this.sliderBody.removeClass("sder-bck");
 			}else if( this.videoStatus == "after"){
 				$fixedEl.removeClass("fixed-el-fixed");
 				$fixedEl.addClass("fixed-el-bottom");
-				$(".fixed-slider-area").removeClass("fixed-slider-area-bgBlack");
+				this.sliderBody.removeClass("sder-bck");
 			}
 		}
 	
 	}
+	/*******Video Slider function******/
+
 
 	function showUpImgAni(){
 		showupAniDone = true;
@@ -102,6 +112,79 @@ $(function(){
 			$(".sw--02").find("img").animate({"bottom":"0px"}, 1000, "easeOutBack");
 		});
 	}
+	
+
+	/********progress********/
+	var progressBar = {
+		progressStatus : false,
+		showProgress : function(){
+			$(".fixed-navi").stop().animate({"right":"10px"},500); 
+		},
+		hideProgress : function(){
+			$(".fixed-navi").stop().animate({"right":"-200px"},500); 
+		},
+		setProgress : function(sc){
+			var fullProgress = $(document).height()-$(window).height()-( $(".footer-area").height()+$(".digital-list").height() +$(".common-footer").height());	
+			var ScrollPer = (sc/fullProgress)*100;
+			if( (sc<$(".sec--01").offset().top || sc > fullProgress) && (this.progressStatus == true)){
+				this.progressStatus = false;
+				this.hideProgress();
+			}else if((sc>=$(".sec--01").offset().top && sc <= fullProgress) && (this.progressStatus == false) ){
+				this.progressStatus = true;
+				this.showProgress();
+			}
+
+			if(isMobile==true){
+				$(".progress").css({"width":ScrollPer+"%"});
+			}else {
+				$(".progress").css({"height":ScrollPer+"%"});
+			}	
+		
+		}
+	}
+
+	/*
+	var progressStatus = false;
+	function showProgress(){
+		$(".fixed-navi").stop().animate({"right":"10px"},500); 
+	};
+	function hideProgress(){
+		$(".fixed-navi").stop().animate({"right":"-200px"},500); 
+	};
+	function setProgress(sc){
+		var fullScroll = $(document).height()-$(window).height()-( $(".footer-area").height()+$(".digital-list").height() +$(".common-footer").height());	
+		var ScrollPer = (sc/fullScroll)*100;
+		if( (sc<$(".sec--01").offset().top || sc > fullScroll) && (progressStatus == true)){
+			progressStatus = false;
+			hideProgress();
+		}else if((sc>=$(".sec--01").offset().top && sc <= fullScroll) && (progressStatus == false) ){
+			progressStatus = true;
+			showProgress();
+		}
+		if(isMobile==true){
+			$(".progress").css({"width":ScrollPer+"%"});
+		}else {
+			$(".progress").css({"height":ScrollPer+"%"});
+		}	
+
+	}*/
+	/********progress********/
+
+	/******** 모바일 전용 조정 ********/
+	if(isMobile==true){
+
+	}
+	/******** 모바일 전용 조정 ********/
+
+	function init(){
+		speachCard.makeCard();
+		
+	}
+
+	$(".loading-page").fadeOut(200, function(){
+		init();
+	});
+
 
 	$(window).scroll(function(){
 		var nowScroll = $(window).scrollTop();
@@ -116,10 +199,9 @@ $(function(){
 		if( nowScrollWithCon > $("#IMG_HOLDER_SHOWUP_ANI").offset().top && showupAniDone == false){
 			showUpImgAni();
 		}
-			
 
-	
-	
+		progressBar.setProgress(nowScroll);
+
 	});
 	
 
