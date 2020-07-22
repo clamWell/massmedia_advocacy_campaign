47791e1d7ceeedb816385e0a053f729340c958a0
h62446244! <pepesi258@hanmail.net>
2020-07-22 17:01:04 +0900

    [보호종료아동_애드보카시] * 형태소 네트워크 그래프 d3.js 로 구현 테스트

---
+++ js/wordDataTest.js
@@ -0,0 +1,431 @@
+var NETWORK_DATA = {
+  "nodes": [
+	  {
+		"id": "청소년",
+		"value": 3,
+		"group": null
+	  },
+	  {
+		"id": "장면",
+		"value": 3,
+		"group": "media"
+	  },
+	  {
+		"id": "드라마",
+		"value": 3,
+		"group": "media"
+	  },
+	  {
+		"id": "캐릭터",
+		"value": 4,
+		"group": null
+	  },
+	  {
+		"id": "미디어",
+		"value": 4,
+		"group": "media"
+	  },
+	  {
+		"id": "대중",
+		"value": 4,
+		"group": "media"
+	  },
+	  {
+		"id": "영향",
+		"value": 2,
+		"group": null
+	  },
+	  {
+		"id": "모습",
+		"value": 3,
+		"group": null
+	  },
+	  {
+		"id": "티비",
+		"value": 4,
+		"group": "media"
+	  },
+	  {
+		"id": "영화",
+		"value": 2,
+		"group": "media"
+	  },
+	  {
+		"id": "매체",
+		"value": 6,
+		"group": "media"
+	  },
+	  {
+		"id": "그리다",
+		"value": 5,
+		"group": "act"
+	  },
+	  {
+		"id": "소비",
+		"value": 5,
+		"group": "act"
+	  },
+	  {
+		"id": "이미지",
+		"value": 3,
+		"group": null
+	  },
+	  {
+		"id": "방송",
+		"value": 5,
+		"group": "media"
+	  },
+	  {
+		"id": "비행",
+		"value": 2,
+		"group": "bad"
+	  },
+	  {
+		"id": "주인공",
+		"value": 4,
+		"group": null
+	  },
+	  {
+		"id": "이야기",
+		"value": 5,
+		"group": null
+	  },
+	  {
+		"id": "실제표현",
+		"value": 5,
+		"group": null
+	  },
+	  {
+		"id": "가난",
+		"value": 2,
+		"group": "bad"
+	  },
+	  {
+		"id": "폭력",
+		"value": 3,
+		"group": "bad"
+	  },
+	  {
+		"id": "씌우다",
+		"value": 4,
+		"group": "act"
+	  },
+	  {
+		"id": "묘사",
+		"value": 3,
+		"group": "act"
+	  },
+	  {
+		"id": "불행",
+		"value": 3,
+		"group": "bad"
+	  },
+	  {
+		"id": "부정",
+		"value": 2,
+		"group": "bad"
+	  },
+	  {
+		"id": "인식",
+		"value": 1,
+		"group": null
+	  },
+	  {
+		"id": "불량",
+		"value": 4,
+		"group": "bad"
+	  }
+   ],
+  "links": [
+	  {
+		"source": "청소년",
+		"target": "장면",
+		"value": 2
+	  },
+	  {
+		"source": "청소년",
+		"target": "비행",
+		"value": 8
+	  },
+	  {
+		"source": "청소년",
+		"target": "티비",
+		"value": 4
+	  },
+	  {
+		"source": "청소년",
+		"target": "모습",
+		"value": 10
+	  },
+	  {
+		"source": "드라마",
+		"target": "캐릭터",
+		"value": 4
+	  },
+	  {
+		"source": "드라마",
+		"target": "미디어",
+		"value": 3
+	  },
+	  {
+		"source": "드라마",
+		"target": "주인공",
+		"value": 2
+	  },
+	  {
+		"source": "드라마",
+		"target": "매체",
+		"value": 1
+	  },
+	  {
+		"source": "드라마",
+		"target": "그리다",
+		"value": 4
+	  },
+	  {
+		"source": "장면",
+		"target": "비행",
+		"value": 5
+	  },
+	  {
+		"source": "장면",
+		"target": "주인공",
+		"value": 1
+	  },
+	  {
+		"source": "이야기",
+		"target": "실제표현",
+		"value": 6
+	  },
+	  {
+		"source": "미디어",
+		"target": "캐릭터",
+		"value": 2
+	  },
+	  {
+		"source": "미디어",
+		"target": "매체",
+		"value": 8
+	  },
+	  {
+		"source": "미디어",
+		"target": "티비",
+		"value": 4
+	  },
+	  {
+		"source": "미디어",
+		"target": "영화",
+		"value": 10
+	  },
+	  {
+		"source": "미디어",
+		"target": "실제표현",
+		"value": 4
+	  },
+	  {
+		"source": "영향",
+		"target": "소비",
+		"value": 3
+	  },
+	  {
+		"source": "영향",
+		"target": "청소년",
+		"value": 2
+	  },
+	  {
+		"source": "영향",
+		"target": "대중",
+		"value": 1
+	  },
+	  {
+		"source": "영향",
+		"target": "미디어",
+		"value": 4
+	  },
+	  {
+		"source": "영향",
+		"target": "매체",
+		"value": 5
+	  },
+	  {
+		"source": "영향",
+		"target": "장면",
+		"value": 1
+	  },
+	  {
+		"source": "비행",
+		"target": "주인공",
+		"value": 6
+	  },
+	  {
+		"source": "비행",
+		"target": "불량",
+		"value": 2
+	  },
+	  {
+		"source": "비행",
+		"target": "실제표현",
+		"value": 8
+	  },
+	  {
+		"source": "가난",
+		"target": "비행",
+		"value": 4
+	  },
+	  {
+		"source": "가난",
+		"target": "주인공",
+		"value": 10
+	  },
+	  {
+		"source": "가난",
+		"target": "폭력",
+		"value": 4
+	  },
+	  {
+		"source": "가난",
+		"target": "이야기",
+		"value": 3
+	  },
+	  {
+		"source": "폭력",
+		"target": "가난",
+		"value": 2
+	  },
+	  {
+		"source": "폭력",
+		"target": "실제표현",
+		"value": 1
+	  },
+	  {
+		"source": "씌우다",
+		"target": "폭력",
+		"value": 4
+	  },
+	  {
+		"source": "씌우다",
+		"target": "영향",
+		"value": 5
+	  },
+	  {
+		"source": "씌우다",
+		"target": "소비",
+		"value": 1
+	  },
+	  {
+		"source": "씌우다",
+		"target": "주인공",
+		"value": 6
+	  },
+	  {
+		"source": "씌우다",
+		"target": "미디어",
+		"value": 2
+	  },
+	  {
+		"source": "씌우다",
+		"target": "캐릭터",
+		"value": 8
+	  },
+	  {
+		"source": "묘사",
+		"target": "씌우다",
+		"value": 4
+	  },
+	  {
+		"source": "묘사",
+		"target": "불행",
+		"value": 10
+	  },
+	  {
+		"source": "묘사",
+		"target": "방송",
+		"value": 4
+	  },
+	  {
+		"source": "묘사",
+		"target": "매체",
+		"value": 3
+	  },
+	  {
+		"source": "소비",
+		"target": "이미지",
+		"value": 2
+	  },
+	  {
+		"source": "소비",
+		"target": "그리다",
+		"value": 1
+	  },
+	  {
+		"source": "소비",
+		"target": "미디어",
+		"value": 4
+	  },
+	  {
+		"source": "소비",
+		"target": "폭력",
+		"value": 5
+	  },
+	  {
+		"source": "매체",
+		"target": "대중",
+		"value": 1
+	  },
+	  {
+		"source": "매체",
+		"target": "영향",
+		"value": 6
+	  },
+	  {
+		"source": "매체",
+		"target": "모습",
+		"value": 2
+	  },
+	  {
+		"source": "매체",
+		"target": "이야기",
+		"value": 3
+	  },
+	  {
+		"source": "매체",
+		"target": "실제표현",
+		"value": 10
+	  },
+	  {
+		"source": "부정",
+		"target": "인식",
+		"value": 10
+	  },
+	  {
+		"source": "부정",
+		"target": "이미지",
+		"value": 3
+	  },
+	  {
+		"source": "부정",
+		"target": "소비",
+		"value": 2
+	  },
+	  {
+		"source": "부정",
+		"target": "불행",
+		"value": 1
+	  },
+	  {
+		"source": "부정",
+		"target": "묘사",
+		"value": 4
+	  },
+	  {
+		"source": "부정",
+		"target": "씌우다",
+		"value": 5
+	  },
+	  {
+		"source": "부정",
+		"target": "이야기",
+		"value": 1
+	  }
+	]
+};
